4c852536bb4ea617d64154ae7d4cdc95f5cffa1e
vincevandeursen <vincevandeursen@gmail.com>
2022-09-29 14:32:52 +0200

    CBS - collision detection complete

--- cbs.py
+++ cbs.py
@@ -1,10 +1,20 @@
+import itertools
 import time as timer
 import heapq
 import random
 from single_agent_planner import compute_heuristics, a_star, get_location, get_sum_of_cost
 
 
-def detect_collision(path1, path2):
+def detect_collision(path1: list, path2: list) -> tuple:
+    """"For two anonymous paths, return the location and timestep for the first collision. That can be either a
+    vertex collision or edge collision.
+
+    :param path1: path of agent 1
+    :param path2: path of agent 2
+
+    :return (col_loc, col_time): col_loc is either a tuple for a vertex collision, or a list of two tuples for an edge
+    collision. Both col_loc and time are None if no collisions found
+    """
     ##############################
     # Task 3.1: Return the first collision that occurs between two robot paths (or None if there is no collision)
     #           There are two types of collisions: vertex collision and edge collision.
@@ -12,7 +22,25 @@ def detect_collision(path1, path2):
     #           An edge collision occurs if the robots swap their location at the same timestep.
     #           You should use "get_location(path, t)" to get the location of a robot at time t.
 
-    pass
+    col_loc = None
+    col_time = None
+
+    for time in range(max(len(path1), len(path2))):
+        a1_loc1 = get_location(path1, time)
+        a1_loc2 = get_location(path1, time + 1)
+        a2_loc1 = get_location(path2, time)
+        a2_loc2 = get_location(path2, time + 1)
+
+        if a1_loc1 == a2_loc1:
+            col_loc = a1_loc1
+            col_time = time
+            break
+        elif a1_loc1 == a2_loc2 and a2_loc1 == a1_loc2:
+            col_loc = [a1_loc1, a1_loc2]
+            col_time = time + 1
+            break
+
+    return col_loc, col_time
 
 
 def detect_collisions(paths):
@@ -22,8 +50,21 @@ def detect_collisions(paths):
     #           causing the collision, and the timestep at which the collision occurred.
     #           You should use your detect_collision function to find a collision between two robots.
 
-    pass
+    collisions = []
+    for a1, a2 in itertools.combinations(range(len(paths)), 2):
+        col_loc, col_time = detect_collision(path1=paths[a1], path2=paths[a2])
 
+        if col_loc is not None:
+            collisions.append(
+                {
+                    'a1': a1,
+                    'a2': a2,
+                    'loc': col_loc,
+                    'timestep': col_time
+                }
+            )
+
+    return collisions
 
 def standard_splitting(collision):
     ##############################
