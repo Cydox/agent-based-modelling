3d4ba1ca144c493e7a4e3fe474237dbd728679ec
Jan Hendrik Farr <github@jfarr.cc>
2022-11-10 13:22:38 +0100

    implement interface of agents

--- distributed_agent.py
+++ distributed_agent.py
@@ -7,7 +7,7 @@ Code in this file is just provided as guidance, you are free to deviate from it.
 class AgentDistributed(object):
     """Aircraft object to be used in the distributed planner."""
 
-    def __init__(self, my_map, start, goal, heuristics, agent_id):
+    def __init__(self, environment, my_map, start, goal, heuristics):
         """
         my_map   - list of lists specifying obstacle positions
         starts      - (x1, y1) start location
@@ -18,5 +18,27 @@ class AgentDistributed(object):
         self.my_map = my_map
         self.start = start
         self.goal = goal
-        self.id = agent_id
         self.heuristics = heuristics
+
+        self.plan = []
+        self.path_history = []
+        self.location = start
+        self.planned_cost = 0
+        self.start = start
+        self.goal = goal
+        self.neighbors = []
+
+    def timestep():
+        pass
+
+    def set_neighbors(self, neighbors):
+        self.neighbors = neighbors
+
+    def update_plan():
+        pass
+
+    def __initial_planning(self):
+        pass
+
+    def __a_star(self, constraints):
+        pass
